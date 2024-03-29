0d36620e090e9caf284b102efe6020f7d634f0c4
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-29 21:42:18 +0300

    alican cli

---
+++ Cli/dist/index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const yargs_1 = __importDefault(require("yargs"));
+const fs_1 = __importDefault(require("fs"));
+const child_process_1 = require("child_process");
+const argv = yargs_1.default
+    .command("create", "create a new file", {
+    "name": {
+        describe: "File Name",
+        demandOption: true,
+        type: "string",
+        alias: 'n'
+    }
+})
+    .command("build", "build cli")
+    .help()
+    .argv;
+if (argv._.includes("create")) {
+    let fileName = argv.name;
+    if (!fileName)
+        fileName = "style";
+    const example = `*{
+    margin:0;
+    padding:0;
+  }`;
+    fs_1.default.writeFileSync(fileName + ".css", example),
+        console.log("css dosyası başarıyla oluşuruldu");
+}
+if (argv._.includes("build")) {
+    (0, child_process_1.exec)("npm run build", (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${stdout}`);
+        }
+        console.log(`stdout:${stdout}`);
+        console.log(`stderr:${stderr}`);
+    });
+}
---
+++ Cli/src/index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import yargs from 'yargs';
+import fs from 'fs';
+import { exec } from 'child_process';
+
+const argv = yargs
+    .command("create", "create a new file", {
+        "name": {
+            describe: "File Name",
+            demandOption: true,
+            type: "string",
+            alias: 'n'
+        }
+    })
+    .command("build", "build cli")
+    .help()
+    .argv as { [key: string]: unknown, _: string[] }
+if (argv._.includes("create")) {
+    let fileName = argv.name as string;
+    if (!fileName) fileName = "style";
+    const example = `*{
+    margin:0;
+    padding:0;
+  }`
+    fs.writeFileSync(fileName + ".css", example),
+        console.log("css dosyası başarıyla oluşuruldu");
+}
+if (argv._.includes("build")) {
+    exec("npm run build", (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${stdout}`)
+        }
+        console.log(`stdout:${stdout}`);
+        console.log(`stderr:${stderr}`);
+
+    });
+}
